57e483162f67ad158e3951da00b38b5637e31265
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-05-08 20:48:54 +0300

    fixed the basic test to use bare objects

--- test/msgpack_test.exs
+++ test/msgpack_test.exs
@@ -4,21 +4,8 @@ defmodule MsgPackObjectTest do
 
     test "basic test" do
         message = <<0x82, 0xa7, 0x63, 0x6f, 0x6d, 0x70, 0x61, 0x63, 0x74, 0xc3, 0xa6, 0x73, 0x63, 0x68, 0x65, 0x6d, 0x61, 0x00>>
-        object = %MsgPackObject{
-            type: :map,
-            value: %{
-                %MsgPackObject{type: :string, value: "compact"} => %MsgPackObject{
-                    type: :boolean,
-                    value: true
-                },
-                %MsgPackObject{type: :string, value: "schema"} => %MsgPackObject{
-                    type: :integer,
-                    value: 0
-                }
-            }
-        }
-        bare_object = %{"compact" => true, "schema" => 0}
-        assert MsgPackObject.parse(message) == object
-        assert MsgPackObject.serialize(bare_object) == message
+        object = %{"compact" => true, "schema" => 0}
+        assert MsgPackObject.parse(message) |> MsgPackObject.get_bare_object == object
+        assert MsgPackObject.serialize(object) == message
     end
 end
